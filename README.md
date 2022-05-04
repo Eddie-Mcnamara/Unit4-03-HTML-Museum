7532c91c41c35d8a0320ffa3bb866ac9e4030efb
Edward.M <99274690+Eddie-Mcnamara@users.noreply.github.com>
2022-05-04 22:16:22 +0000

    Completed unit4-04 including favicon

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Unit4-03-HTML-Museum
+[![GitHub's Super Linter](https://github.com/Eddie-Mcnamara/Unit4-03-HTML-Museum/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/Eddie-Mcnamara/Unit4-03-HTML-Museum/actions)
---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Copyright (c) 2022 Edward Mcnamara All rights reserved
+//
+// Created by: Edward Mcnamara
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+'use strict';
+/**
+ * This function calculates which rating of movie the user is allowed to watch based on their age 
+ */ 
+function check() {
+  // Getting age from user
+  let age = parseInt(document.getElementById('userAge').value)
+  let select = document.getElementById('days')
+  let day = select.options[select.selectedIndex].value
+  let result;
+
+  
+ if (age < 0) {
+  result = "You have entered an invalid age, please try again."
+ } else if (age <= 5 || age >= 95 ) {
+  result = "We offer free admission to visitors 5 and under and 95 and older! Because you're " + age + ", you're eligible for this discount!";
+} else if (day == "tuesday" || day == "thursday") {
+  result = "Congratulations! you get a discount because today is " + day + ". Unfortunately  there is a maximum of 1 discount per person. So if you're a student, you may only get the discount related to the day of the week" 
+} else if (age >= 12 && age <= 21) {
+  result = "Congratulations! You get a discount because you are a student of age " + age + " . Please redeem your discount at the entrance fee station"
+} else {
+  result = "You pay the regular price. There are no discounts for you today. Come back on Tuesday or Thursday to get yourself a discount :)"
+}  
+  // Displaying results 
+  document.getElementById('result').innerHTML = result
+}
